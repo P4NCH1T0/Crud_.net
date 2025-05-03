6fa77fa2a143a182ce717b234c2620625af35c50
P4NCH1T0 <98226655+P4NCH1T0@users.noreply.github.com>
2025-05-02 20:22:22 -0600

    Creacion de tablas

---
+++ Crud/Crud/Repositories/ClientRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crud.Repositories
+{
+    public class ClientRepository
+    {
+        private readonly string connectionString = "";
+    }
+}
